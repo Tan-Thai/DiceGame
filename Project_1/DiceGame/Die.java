9f35f825052cf0802733af5b248ffa27a89363a7
Tan-Thai <Vi.Tan.Thai@gmail.com>
2024-09-23 14:32:32 +0200

    Added skeleton for dicegame

---
+++ Project_1/DiceGame/Die.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+
+public class Die {
+    private int numberOfSides;
+    private int currentValue;
+    private Random random = new Random();
+
+    // getters, setters
+    public int getNumberOfSides() {
+        return numberOfSides;
+    }
+    public int getCurrentValue() {
+        return currentValue;
+    }
+
+    // Constructors
+    public Die(int numberOfSides) {
+        this.numberOfSides = numberOfSides;
+    }
+
+    // Methods
+    public void roll() {
+        currentValue = random.nextInt(1, numberOfSides);
+    }
+
+}
---
+++ Project_1/DiceGame/Player.java
@@ -0,0 +1,36 @@
+public class Player {
+    String name;
+    int score = 0; 
+    Die die;
+    
+    //getters, setters
+    public String getName() {return name;}
+
+    public int getScore() {return score;}
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    //constructors 
+    public Player(String name) {
+        this.name = name;
+    }
+
+    //methods
+    public void rollDice() {
+        this.die.roll();
+        System.out.println(this.die.getCurrentValue());
+    }
+
+    public int getDieValue() {
+        return this.die.getCurrentValue();
+    }
+
+    public void increaseScore() {
+        this.score++;
+    }
+
+    public void addDie(int numberOfSides) {
+        this.die = new Die(numberOfSides);
+    }
+}
